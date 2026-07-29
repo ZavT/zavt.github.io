450dd5bb9a5476fe124430af44ea3adf0fcff9a0
Zavier <zavier08tan@gmail.com>
2026-07-29 22:54:57 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,141 +1,298 @@
-//Ball 
-//target all elements to save to constants
-const page1btn=document.querySelector("#page1btn");
-const page2btn=document.querySelector("#page2btn");
-const page3btn=document.querySelector("#page3btn");
-const page1=document.querySelector("#page1");
-const page2=document.querySelector("#page2");
-const page3=document.querySelector("#page3");
-function hideall(){ //function to hide all pages
-page1.style.display="none";
-page2.style.display="none";
-page3.style.display="none";
-}
+//load finish than let code run
+document.addEventListener("DOMContentLoaded", function() {
 
-/*Listen for clicks on the buttons, assign anonymous
-eventhandler functions to call show function*/
-page1btn.addEventListener("click", function () {
-hideall(); //we don't know which page is shown, so hideall
-page1.style.display="block";
+/*Navbar*/
+const navMenu = document.getElementById("nav-menu");
+const pages = document.querySelectorAll(".page");
+const navButtons = document.querySelectorAll(".nav-btn");
+
+navMenu.addEventListener("click", function(event) {
+   
+	if (event.target.tagName === "BUTTON") {
+		
+		const targetPageId = event.target.getAttribute("data-target");
+	   
+		pages.forEach(function(page) {
+			page.classList.remove("active");
+			page.classList.add("hidden");
+		});
+		
+		navButtons.forEach(function(btn) {
+			btn.classList.remove("active-btn");
+		});
+
+		// hightligh current page button
+		document.getElementById(targetPageId).classList.remove("hidden");
+		document.getElementById(targetPageId).classList.add("active");
+		event.target.classList.add("active-btn");
+	}
 });
-page2btn.addEventListener("click", function () {
-hideall(); //we don't know which page is shown, so hideall
-page2.style.display="block";
+	
+/*Quiz*/
+let score = 0;
+let currentTab = 0; 
+
+showTab(currentTab); 
+
+document.getElementById("prevBtn").addEventListener("click", function() {
+	nextPrev(-1);
 });
-page3btn.addEventListener("click", function () {
-hideall(); //we don't know which page is shown, so hideall
-page3.style.display="block";
+document.getElementById("nextBtn").addEventListener("click", function() {
+	nextPrev(1);
 });
-hideall(); //call hideall function to hide all pages
+document.getElementById("resetBtn").addEventListener("click", resetQuiz);
 
+document.getElementById("redoBtn").addEventListener("click", resetQuiz);
 
-/*find references to all the buttons and ball */
-const leftBtn = document.querySelector("#leftBtn");
-const rightBtn = document.querySelector("#rightBtn");
-const upBtn = document.querySelector("#upBtn");
-const downBtn = document.querySelector("#downBtn");
-const resetBtn = document.querySelector("#resetBtn");
-const ball = document.querySelector("#ball");
-var ballX = ballY = 0; //assign initial position of ball
-function ResetPos() {
-ballX=ballY=0; //reset to zero
-ball.style.left = ballX+"px"; //set left property to ball x variable
-ball.style.top = ballY+"px"; //set top property to ball x variable
-ball.innerText = ballX + "," + ballY; //update ball text to show coordinate
+    // show curr tab
+    function showTab(n) {
+        let tabs = document.getElementsByClassName("tab");
+        tabs[n].style.display = "block";
+        
+        // prev shows up after first qn
+        if (n === 0) {
+            document.getElementById("prevBtn").style.display = "none";
+        } else {
+            document.getElementById("prevBtn").style.display = "inline";
+        }
+        
+        // submit button for the last qn
+        if (n === (tabs.length - 1)) {
+            document.getElementById("nextBtn").innerHTML = "Submit";
+        } else {
+            document.getElementById("nextBtn").innerHTML = "Next";
+        }
+        
+        //show progress dots
+        fixStep(n);
 }
-function MovePos(leftInc, topInc) {
-ballX =ballX+ leftInc;
-ballY =ballY+ topInc;
-ball.style.left = ballX+"px"; //set left css property to ball x variable
-ball.style.top = ballY+"px"; //set top css property to ball y variable
-ball.innerText = ballX + "," + ballY; //update ball text to show coordinate
-}
-function MoveLeft(){
-ballX =ballX-10; //decrement by 10
-ballY =ballY+0; //no change
-ball.style.left = ballX+"px"; //set left css property to ball x variable
-ball.style.top = ballY+"px"; //set top css property to ball y variable
-ball.innerText = ballX + "," + ballY; //update ball text to show coordinate
-}
-//eventlistener to activate MoveLeft (named callback function)
-leftBtn.addEventListener("click", MoveLeft); //no brackets after MoveLeft
-//eventListener to anonymous callback function (other way)
-rightBtn.addEventListener("click", function () {
-MovePos(10, 0);
+
+    //next qns or prev qns
+    function nextPrev(n) {
+        let tabs = document.getElementsByClassName("tab");
+        
+        //answer qns before going next 
+        if (n === 1 && !checkForm()) {
+            return false;
+        }
+        
+        tabs[currentTab].style.display = "none";
+        currentTab = currentTab + n;
+        
+        //check if end of quiz
+        if (currentTab >= tabs.length) {
+            markAns(); //calculate the final score
+            document.getElementById("Buttons").style.display = "none"; // Hide Prev & Next
+            return false;
+        }
+        
+        // if not show the next qns
+        showTab(currentTab);
+    }
+
+    //an answer has been sleceted 
+    function checkForm() {
+        let tabs = document.getElementsByClassName("tab");
+        let radios = tabs[currentTab].querySelectorAll("input[type='radio']");
+        let isValid = false;
+        
+        for (let i = 0; i < radios.length; i++) {
+            if (radios[i].checked) {
+                isValid = true;
+                break;
+            }
+        }
+        
+        if (isValid) {
+            document.getElementsByClassName("step")[currentTab].className += " finish";
+        } else {
+            alert("Please select an answer to continue!");
+        }
+        
+        return isValid; 
+    }
+
+    //show progress
+    function fixStep(n) {
+        let steps = document.getElementsByClassName("step");
+        for (let i = 0; i < steps.length; i++) {
+            steps[i].className = steps[i].className.replace(" active", "");
+        }
+        if(steps[n]) {
+            steps[n].className += " active";
+        }
+    }
+
+    //calculates the score
+    function markAns() {
+        score = 0; 
+        const scorebox = document.getElementById("scorebox");
+        function getAnswer(qnsName) {
+            const selected = document.querySelector("input[name='" + qnsName + "']:checked");
+            return selected ? selected.value : "";
+        }
+
+        if (getAnswer("q1") === "Fighting") score++;
+        if (getAnswer("q2") === "Painting") score++;
+        if (getAnswer("q3") === "cry") score++;
+        if (getAnswer("q4") === "Tired") score++;
+        if (getAnswer("q5") === "8") score++;
+        scorebox.innerHTML = "You scored: " + score + " out of 5!";
+		
+		document.getElementById("redoBtn").style.display = "block";
+    }
+
+    //uncheck all radio and back to first qns
+    function resetQuiz() {
+        let tabs = document.getElementsByClassName("tab");
+        for (let i = 0; i < tabs.length; i++) {
+            tabs[i].style.display = "none";
+        }
+
+        //uncheck all radio buttons
+        let radios = document.querySelectorAll("input[type='radio']");
+        for (let i = 0; i < radios.length; i++) {
+            radios[i].checked = false;
+        }
+
+        //remove all progress
+        let steps = document.getElementsByClassName("step");
+        for (let i = 0; i < steps.length; i++) {
+            steps[i].className = steps[i].className.replace(" finish", "");
+            steps[i].className = steps[i].className.replace(" active", "");
+        }
+
+        document.getElementById("scorebox").innerHTML = "Not submitted";
+        document.getElementById("Buttons").style.display = "block"; 
+
+        //restart from the beginning
+        currentTab = 0;
+        showTab(currentTab);
+    }
+	
+/*Mini game*/
+const character = document.getElementById("character");
+const block = document.getElementById("block");
+const scoreDisplay = document.getElementById("gameScore");
+const timerDisplay = document.getElementById("gameTimer");
+const highScoreDisplay = document.getElementById("highScore");
+const startBtn = document.getElementById("startBtn");
+
+const jumpAudio = new Audio("audio/jump.mp3");
+const loseAudio = new Audio("audio/lose.mp3");
+
+let gameScore = 0;
+let highScore = 0;
+let timeLeft = 30;
+let GameOver = true; 
+let timerInterval;
+
+//start 
+startBtn.addEventListener("click", startGame);
+
+	function startGame() {
+		GameOver = false;
+		gameScore = 0;
+		timeLeft = 30;
+		
+		scoreDisplay.innerHTML = "Score: " + gameScore;
+		timerDisplay.innerHTML = "Time: " + timeLeft + "s";
+		startBtn.style.display = "none"; 
+		
+		// fence starts moving
+		block.style.animation = "block 1.5s infinite linear";
+		
+		//timer
+		clearInterval(timerInterval);
+		timerInterval = setInterval(function() {
+			timeLeft--;
+			timerDisplay.innerHTML = "Time: " + timeLeft + "s";
+			
+			//end time 
+			if (timeLeft <= 0) {
+				endGame();
+			}
+		}, 1000);
+	}
+
+	function endGame() {
+		GameOver = true;
+		clearInterval(timerInterval);
+		block.style.animation = "none"; 
+		
+		//update high score
+		if (gameScore > highScore) {
+			highScore = gameScore;
+			highScoreDisplay.innerHTML = "High Score: " + highScore;
+		}
+		
+		startBtn.innerHTML = "Play Again";
+		startBtn.style.display = "inline-block"; 
+	}
+
+	// jump click (desktop), tap (mobile)
+	document.addEventListener("click", jump);
+	document.addEventListener("touchstart", jump);
+
+	function jump(event) {
+		
+		if (event.target.id === "startGameBtn") return;
+
+		// check if in game tab
+		const gamePage = document.getElementById("game");
+		if (!gamePage.classList.contains("active")) {
+			return; 
+		}
+		
+		//no jumping again if in animation or game 
+		if (character.classList.contains("animate") || GameOver) { 
+			return; 
+		}
+		
+		jumpAudio.play();
+		character.classList.add("animate");
+		
+		setTimeout(function() {
+			character.classList.remove("animate");
+		}, 400); 
+	}
+
+	//
+	block.addEventListener('animationiteration', function() {
+		if (!GameOver) {
+			gameScore++;
+			scoreDisplay.innerHTML = "Score: " + gameScore;
+		}
+	});
+
+	//Collision 
+	setInterval(function() {
+		if (GameOver) return; 
+
+		let characterRect = character.getBoundingClientRect();
+		let blockRect = block.getBoundingClientRect();
+		
+		//check sheep touching fence
+		if (
+			characterRect.right > blockRect.left &&
+			characterRect.left < blockRect.right &&
+			characterRect.bottom > blockRect.top &&
+			characterRect.top < blockRect.bottom
+		) {
+			//pause animation to reset block
+			block.style.animation = "none";
+			void block.offsetWidth; 
+			
+			loseAudio.play();
+			
+			if (gameScore > 0) {
+				gameScore--;
+			}
+			scoreDisplay.innerHTML = "Score: " + gameScore;
+			
+			
+			block.style.animation = "block 1.5s infinite linear";
+		}
+	}, 10);
+
 });
-upBtn.addEventListener("click", function () {
-MovePos(0, -10);
-});
-downBtn.addEventListener("click", function () {
-MovePos(0, 10);
-});
-resetBtn.addEventListener("click", ResetPos);
-
-document.addEventListener('keydown', function (kbEvt) {
-//kbEvt: an event object passed to callback function
-console.log(kbEvt); //see what is returned
-if (kbEvt.code === "ArrowRight"){
-MovePos(10,0);
-}
-if (kbEvt.code === "ArrowLeft"){
-MoveLeft();
-}
-if (kbEvt.code === "ArrowDown"){
-MovePos(0, 10);
-}
-if (kbEvt.code === "ArrowUp"){
-MovePos(0, -10);
-}
-//Better option: use switch case instead
-});
-
-
-// Durian game
-const durianId = document.getElementById("durianId");
-
-function GetRandom(min,max){
-	//this will select a number between min and max
-return Math.round(Math.random() * (max - min)) + min;
-}
-
-function MoveDurian() {
-durianId.style.left = GetRandom(0, 500) + "px";
-durianId.style.top = GetRandom(0, 500) + "px";
-}
-
-var moveDurianItvId = setInterval(MoveDurian, 1000);
-
-const scoreBox=document.getElementById("scoreBox");
-const popAudio = new Audio("popsound.mp3");
-
-var score=0; //to track how many clicks
-function durianCatch() {
-//increases score after clicking
-score++;
-
-//update html scorebox
-scoreBox.innerHTML = "Score: " + score;
-
-//play the audio!
-popAudio.play(); 
-}
-
-//link durian to mouseclick towst durianCatch function
-durianId.addEventListener("click",durianCatch);
-
-document.addEventListener("keydown",function(evt){
-console.log(evt);
-if(evt.code=="KeyT"){
-durianId.classList.add("shrink");
-}
-if(evt.code=="KeyU"){
-durianId.classList.remove("shrink");
-}
-if(evt.code=="KeyA"){
-durianId.classList.add("anim1");
-}
-if(evt.code=="KeyB"){
-durianId.classList.remove("anim1");
-}
-});
-
